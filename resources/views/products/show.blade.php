c1b7bf301050bdcca8c0dc7e1c6264eee305f09e
Alexis <contact@alexis-petit.fr>
2024-05-13 11:58:33 +0200

    delete

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,9 +1,19 @@
 <x-layout :title="$product->name">
     <h1>{{ $product->name }}</h1>
 
-    <a href="{{ route('products.edit', $product) }}" class="btn btn-outline-primary">
-        Modifier
-    </a>
+    <div class="d-flex flex-row gap-2">
+        <div>
+            <a href="{{ route('products.edit', $product) }}" class="btn btn-outline-primary">
+                Modifier
+            </a>
+        </div>
+
+        <form action="{{ route('products.destroy', $product) }}" method="post">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="btn btn-outline-danger">Supprimer</button>
+        </form>
+    </div>
 
     <hr>
 
